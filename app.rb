e5e724189261b059465f628e9e06fc8677227289
buckethead1986 <rspeckpfs@gmail.com>
2017-10-08 16:06:15 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+      "My name is Robert"
+  end
+
+  get '/hometown' do
+    "My hometown is Palo Alto"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Lets Pretend', by No Fi Soul Rebellion"
+  end
+
+
+
 end
